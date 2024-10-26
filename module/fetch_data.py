e4f3224bec4646a1f891e4512084d475392e589d
reda.adardor <reda.adardor@euranova.eu>
2024-10-26 19:43:24 +0200

    fix: :zap: optimize code and add desc

--- module/fetch_data.py
+++ module/fetch_data.py
@@ -2,12 +2,12 @@ from pyopensky.s3 import S3Client
 import subprocess
 
 # Command to move all .parquet files to the 'data' directory
-command = 'mv *.parquet data/'
+command = "mv *.parquet data/"
 
 s3 = S3Client()
 
 for obj in s3.s3client.list_objects("competition-data", recursive=True):
-     print(f"{obj.bucket_name=}, {obj.object_name=}, ")
-     s3.download_object(obj)
-     # Execute the command
-     result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print(f"{obj.bucket_name=}, {obj.object_name=}, ")
+    s3.download_object(obj)
+    # Execute the command
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
